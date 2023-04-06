a20f473bb77188d79ed396d2ea8a693c59c1ae36
sdsartor <sdsartor48@gmail.com>
2023-04-06 13:18:00 -0700

    added quest to the router paths

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const usersRouter = require('./users-router');
 const exampleRouter = require('./example-router');
+const questRouter = require('./quest-router');
 
 router.use('/users', usersRouter);
 router.use('/example', exampleRouter);
+router.use('/quests', questRouter);
 
 
 module.exports = router;
